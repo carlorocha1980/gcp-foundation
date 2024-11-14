ca2aa0caabea670432a2b6f138b176d240f4909c
mvsouza34 <marcos.souza@ipnet.cloud>
2024-11-14 20:20:32 -0300

    Adding

--- 02-network/shared-vpc.tf
+++ 02-network/shared-vpc.tf
@@ -1,37 +1,37 @@
-# # # google_compute_shared_vpc_service_project.service_project_aiqfome_dev
+# # google_compute_shared_vpc_service_project.service_project_aiqfome_dev
 
-# # # Enable shared VPC for the host project.
-# # # gcloud beta compute shared-vpc enable network-host-project-dev
-# # #
-# # # Enable Shared-VPC, and add the private networt and dmz network in host project network_host_project
+# # Enable shared VPC for the host project.
+# # gcloud beta compute shared-vpc enable network-host-project-dev
+# #
+# # Enable Shared-VPC, and add the private networt and dmz network in host project network_host_project
 
 
-# # Create a resource shared VPC network in the host project.
-# resource "google_compute_shared_vpc_host_project" "shared_vpc_host_project" {
-#   project = local.projects.host_project.name
-# }
+# Create a resource shared VPC network in the host project.
+resource "google_compute_shared_vpc_host_project" "shared_vpc_host_project" {
+  project = var.host_project_id
+}
 
-# # Define the shared VPC network in the service project aiqfome_project_id
-# resource "google_compute_shared_vpc_service_project" "service_project_dev" {
-#   service_project = local.projects.service_project.name
+# Define the shared VPC network in the service project aiqfome_project_id
+resource "google_compute_shared_vpc_service_project" "service_project_dev" {
+  service_project = var.service_project_id
+  host_project    = var.host_project_id
+
+  depends_on = [ google_compute_network.vpc_network_avantia_dev,
+                 google_compute_shared_vpc_host_project.shared_vpc_host_project ]
+}
+# # Define the shared VPC network in the service project aiqentrega_project_id
+# resource "google_compute_shared_vpc_service_project" "aiqentrega_service_project_dev" {
+#   service_project = local.projects.service_project_02.name
 #   host_project    = local.projects.host_project.name
 
-#   depends_on = [ google_compute_network.vpc_network_avantia_dev,
-#                  google_compute_shared_vpc_host_project.shared_vpc_host_project ]
+#   depends_on = [ google_compute_network.vpc_network_aiqentrega_dev,
+#                  google_compute_shared_vpc_host_project.shared_vpc_host_project_dev ]
 # }
-# # # Define the shared VPC network in the service project aiqentrega_project_id
-# # resource "google_compute_shared_vpc_service_project" "aiqentrega_service_project_dev" {
-# #   service_project = local.projects.service_project_02.name
-# #   host_project    = local.projects.host_project.name
+# # Define the shared VPC network in the service project plusdelivery_project_id
+# resource "google_compute_shared_vpc_service_project" "plusdelivery_service_project_dev" {
+#   service_project = projects.service_project_03.name
+#   host_project    = local.projects.host_project.name
 
-# #   depends_on = [ google_compute_network.vpc_network_aiqentrega_dev,
-# #                  google_compute_shared_vpc_host_project.shared_vpc_host_project_dev ]
-# # }
-# # # Define the shared VPC network in the service project plusdelivery_project_id
-# # resource "google_compute_shared_vpc_service_project" "plusdelivery_service_project_dev" {
-# #   service_project = projects.service_project_03.name
-# #   host_project    = local.projects.host_project.name
-
-# #   depends_on = [ google_compute_network.vpc_network_plusdelivery_dev,
-# #                  google_compute_shared_vpc_host_project.shared_vpc_host_project_dev ]
-# # }
+#   depends_on = [ google_compute_network.vpc_network_plusdelivery_dev,
+#                  google_compute_shared_vpc_host_project.shared_vpc_host_project_dev ]
+# }
